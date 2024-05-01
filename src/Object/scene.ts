e77b25eb64d5dbc7f2518e86cd8606ed100e0601
Fakhrimm <13521045@std.stei.itb.ac.id>
2024-05-01 20:53:15 +0700

    feat: add dummy in scene

--- src/Object/scene.ts
+++ src/Object/scene.ts
@@ -5,4 +5,18 @@ export class Scene extends Node {
         super()
         this.parent = null
     }
+
+    public static createSceneDummy(): Scene {
+        const scene = new Scene()
+
+        
+        const node1 = new Node()
+        const node2 = new Node()
+        const node3 = new Node()
+
+        node1.setParent(scene)
+        node2.setParent(scene)
+        node3.setParent(node1)
+        return scene
+    }
 }
