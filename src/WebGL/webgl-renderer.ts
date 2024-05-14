2e970a0afb9080ae68dc2ed2679f1d36cb7f78d2
AustinPardosi <gabrielpardosi26@gmail.com>
2024-05-14 15:47:09 +0700

    feat: add element listener

--- src/WebGL/webgl-renderer.ts
+++ src/WebGL/webgl-renderer.ts
@@ -1,86 +1,98 @@
-import {Camera} from "../Camera/camera";
-import {OrthographicCamera} from "../Camera/orthographicCamera";
-import {Scene} from "../Object/scene";
-import {ProgramInfo} from "./program-info";
-import {Matrix4} from "../Math/matrix-4.ts";
-import {Mesh} from "../Object/mesh.ts";
+import { Camera } from "../Camera/camera";
+import { OrthographicCamera } from "../Camera/orthographicCamera";
+import { Scene } from "../Object/scene";
+import { ProgramInfo } from "./program-info";
+import { Matrix4 } from "../Math/matrix-4.ts";
+import { Mesh } from "../Object/mesh.ts";
 
 export class WebGLRenderer {
-    canvas: HTMLCanvasElement
-    gl: WebGLRenderingContext
-    programInfo: ProgramInfo
-    constructor(canvas: HTMLCanvasElement, programInfo: ProgramInfo) {
-        this.canvas = canvas
-        const gl = this.canvas.getContext("webgl")
-        if (!gl) {
-            throw new Error("No webgl support")
-        }
-        this.gl = gl
-        this.programInfo = programInfo;
+  canvas: HTMLCanvasElement;
+  gl: WebGLRenderingContext;
+  programInfo: ProgramInfo;
+  constructor(canvas: HTMLCanvasElement, programInfo: ProgramInfo) {
+    this.canvas = canvas;
+    const gl = this.canvas.getContext("webgl");
+    if (!gl) {
+      throw new Error("No webgl support");
+    }
+    this.gl = gl;
+    this.programInfo = programInfo;
+  }
+
+  adjustCanvas() {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    const displayWidth = this.canvas.clientWidth;
+    const displayHeight = this.canvas.clientHeight;
+
+    // Check if the canvas is not the same size.
+    const needResize =
+      this.canvas.width !== displayWidth ||
+      this.canvas.height !== displayHeight;
+
+    if (needResize) {
+      // Make the canvas the same size
+      this.canvas.width = displayWidth;
+      this.canvas.height = displayHeight;
+    }
+    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  render(scene: Scene) {
+    this.adjustCanvas();
+    this.gl.clearColor(0, 0, 0, 0);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.gl.clear(this.gl.DEPTH_BUFFER_BIT);
+    this.gl.useProgram(this.programInfo.program);
+
+    const camera = this.getCamera(scene, OrthographicCamera);
+    if (!camera) {
+      throw new Error("No camera found in the scene");
     }
 
-    adjustCanvas() {
-        // Lookup the size the browser is displaying the canvas in CSS pixels.
-        const displayWidth  = this.canvas.clientWidth;
-        const displayHeight = this.canvas.clientHeight;
-        
-        // Check if the canvas is not the same size.
-        const needResize = this.canvas.width  !== displayWidth ||
-                            this.canvas.height !== displayHeight;
-        
-        if (needResize) {
-            // Make the canvas the same size
-            this.canvas.width  = displayWidth;
-            this.canvas.height = displayHeight;
-        }
-        this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-    }
+    scene.updateWorldMatrix(new Matrix4().identity());
 
-    render(scene: Scene) {
-        this.adjustCanvas();
-        this.gl.clearColor(0, 0, 0, 0);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-        this.gl.clear(this.gl.DEPTH_BUFFER_BIT);
-        this.gl.useProgram(this.programInfo.program);
+    this.programInfo.setUniforms({
+      u_viewProjection: camera.viewProjectionMatrix.elements,
+    });
 
-        const camera = this.getCamera(scene, OrthographicCamera)
-        if (!camera) {
-            throw new Error("No camera found in the scene");
-        }
-
-        scene.updateWorldMatrix(new Matrix4().identity());
-
+    scene.traverse((node) => {
+      if (node instanceof Mesh) {
+        const geometry = node.geometry;
+        this.programInfo.setAttributesAndIndices(geometry);
         this.programInfo.setUniforms({
-            u_viewProjection: camera.viewProjectionMatrix.elements
+          u_world: node.getWorldMatrix().elements,
         });
+        if (geometry.indices) {
+          this.gl.drawElements(
+            this.gl.TRIANGLES,
+            geometry.indices.length,
+            this.gl.UNSIGNED_SHORT,
+            0
+          );
+        } else {
+          this.gl.drawArrays(
+            this.gl.TRIANGLES,
+            0,
+            geometry.attributes.a_position.data.length / 4
+          );
+        }
+      }
+    });
+  }
 
+  getCamera(
+    scene: Scene,
+    cameraType: new (...args: any[]) => Camera
+  ): Camera | null {
+    let foundCamera: Camera | null = null;
+    scene.traverseWithTotalBreak((node) => {
+      if (node instanceof cameraType) {
+        foundCamera = node as Camera;
+        return false; // Found the camera, stop traversal
+      }
+      return true; // Continue traversal otherwise
+    });
 
-        scene.traverse(node => {
-            if (node instanceof Mesh) {
-                const geometry = node.geometry;
-                this.programInfo.setAttributesAndIndices(geometry);
-                this.programInfo.setUniforms({
-                    u_world: node.getWorldMatrix().elements
-                });
-                if (geometry.indices) {
-                    this.gl.drawElements(this.gl.TRIANGLES, geometry.indices.length, this.gl.UNSIGNED_SHORT, 0)
-                } else {
-                    this.gl.drawArrays(this.gl.TRIANGLES, 0, geometry.attributes.a_position.data.length / 4);
-                }
-            }
-        })
-    }
-
-    getCamera(scene: Scene, cameraType: new (...args: any[]) => Camera): Camera | null {
-        let foundCamera: Camera | null = null;
-        scene.traverseWithTotalBreak(node => {
-            if (node instanceof cameraType) {
-                foundCamera = node as Camera;
-                return false;  // Found the camera, stop traversal
-            }
-            return true;  // Continue traversal otherwise
-        });
-
-        return foundCamera;
-    }
+    return foundCamera;
+  }
 }
---
+++ src/utils/ElementListener.ts
@@ -0,0 +1,17 @@
+import { Variables } from "./Variables";
+
+export function elementListner(variables: Variables) {
+  const container = variables.getContainer();
+  // TOP
+  const help = container.getElement("help");
+  const modalContainer = container.getElement("modalContainer");
+  const modalBackdrop = container.getElement("modalBackdrop");
+
+  help.addEventListener("click", () => {
+    modalContainer.style.display = "block";
+  });
+
+  modalBackdrop.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+  });
+}
