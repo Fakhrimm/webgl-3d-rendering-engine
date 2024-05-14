ef076094cc760f7387aa063a2f3705beb93bcc98
Fakhrimm <13521045@std.stei.itb.ac.id>
2024-05-14 15:54:14 +0700

    feat: new sceneDummy

--- src/Object/scene.ts
+++ src/Object/scene.ts
@@ -1,8 +1,9 @@
 import { Node } from "./node";
 import {OrthographicCamera} from "../Camera/orthographicCamera.ts";
 import {Mesh} from "./mesh.ts";
-import {PlaneGeometry} from "../Geometry/planeGeometry.ts";
 import {Material} from "../Material/material.ts";
+import {BoxGeometry} from "../Geometry/boxGeometry.ts";
+import {Vector3} from "../Math/vector-3.ts";
 
 export class Scene extends Node {
     constructor() {
@@ -10,25 +11,27 @@ export class Scene extends Node {
         this.parent = null
     }
 
-    public static createSceneDummy(canvas: HTMLCanvasElement): Scene {
+    public static createSceneDummy(canvas: HTMLCanvasElement | null, now: number): Scene {
+        if (!canvas) {
+            throw new Error("Canvas is null")
+        }
         const scene = new Scene()
         scene.name = "SceneDummy"
 
-        const ortoCamera = new OrthographicCamera(0, canvas.width, canvas.height, 0, -1, 1)
+        const ortoCamera = new OrthographicCamera(-canvas.width/2, canvas.width/2, -canvas.height/2, canvas.height/2, -101, 101)
         ortoCamera.name = "OrthographicCamera"
 
-        const plane = new PlaneGeometry(100, 100)
-        const mesh = new Mesh(plane, new Material())
+        // const plane = new PlaneGeometry(100, 100)
+        // const mesh = new Mesh(plane, new Material())
+
+        const mesh = new Mesh(new BoxGeometry(100, 100, 100), new Material())
         mesh.name = "Mesh"
-        // const node2 = new Node()
-        // node2.name = "Node2"
-        // const node3 = new Node()
-        // node3.name = "Node3"
+
+        mesh.setRotationFromAxisAngle(new Vector3(0.57735026919, 0.57735026919, 0.57735026919), now)
 
         ortoCamera.setParent(scene)
         mesh.setParent(scene)
-        // node2.setParent(scene)
-        // node3.setParent(node1)
+
         return scene
     }
 }
