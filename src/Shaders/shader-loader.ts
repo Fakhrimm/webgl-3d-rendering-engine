11a9343dab801a4922080be5a022afa325f90ff7
Fakhrimm <13521045@std.stei.itb.ac.id>
2024-05-14 07:40:20 +0700

    feat: add new shader

--- src/Shaders/shader-loader.ts
+++ src/Shaders/shader-loader.ts
@@ -1,13 +1,17 @@
-import vertexShaderSource from "./vertex-shader.vert?raw";
-import fragmentShaderSource from "./fragment-shader.frag?raw";
 import vertexBasic from "./vertex-basic.vert?raw";
+import vertexShaderSource from "./vertex-shader.vert?raw";
+import vertexShaderAdv from "./vertex-adv.vert?raw";
 import fragmentBasic from "./fragment-basic.frag?raw";
+import fragmentShaderSource from "./fragment-shader.frag?raw";
+import fragmentShaderAdv from "./fragment-adv.frag?raw";
 
 export enum ShaderType {
     VERTEX_BASIC,
     VERTEX_REGULAR,
+    VERTEX_ADV,
     FRAGMENT_BASIC,
-    FRAGMENT_REGULAR
+    FRAGMENT_REGULAR,
+    FRAGMENT_ADV
 }
 
 
@@ -16,12 +20,16 @@ export function loadShader(shaderType: ShaderType): string {
     switch (shaderType) {
         case ShaderType.VERTEX_BASIC:
             return vertexBasic
-        case ShaderType.FRAGMENT_BASIC:
-            return fragmentBasic
         case ShaderType.VERTEX_REGULAR:
             return vertexShaderSource
+        case ShaderType.VERTEX_ADV:
+            return vertexShaderAdv
+        case ShaderType.FRAGMENT_BASIC:
+            return fragmentBasic
         case ShaderType.FRAGMENT_REGULAR:
             return fragmentShaderSource
+        case ShaderType.FRAGMENT_ADV:
+            return fragmentShaderAdv
         default:
             throw new Error("ShaderType not implemented in loader");
     }
--- src/Shaders/vertex-basic.vert
+++ src/Shaders/vertex-basic.vert
@@ -3,8 +3,7 @@ attribute vec4 a_position;
 
 // all shaders have a main function
 void main() {
-
-// gl_Position is a special variable a vertex shader
-// is responsible for setting
-gl_Position = a_position;
+    // gl_Position is a special variable a vertex shader
+    // is responsible for setting
+    gl_Position = a_position;
 }
