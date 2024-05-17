e9811be76a4433e3ba10478a62a81f9421fd9b4b
Fakhrimm <13521045@std.stei.itb.ac.id>
2024-05-17 10:18:34 +0700

    chore: correct line spaces

--- src/Camera/orthographicCamera.ts
+++ src/Camera/orthographicCamera.ts
@@ -2,40 +2,40 @@ import { CameraType } from "../Types/camera-types";
 import {Camera} from "./camera";
 
 export class OrthographicCamera extends Camera {
-  top: number;
-  bottom: number;
-  left: number;
-  right: number;
-  near: number;
-  far: number;
-  zoom: number;
+	  top: number;
+	  bottom: number;
+	  left: number;
+	  right: number;
+	  near: number;
+	  far: number;
+	  zoom: number;
 
-	constructor(left = - 1, right = 1, top = 1, bottom = - 1, near = 0.1, far = 2000) {
+		constructor(left = - 1, right = 1, top = 1, bottom = - 1, near = 0.1, far = 2000) {
 
-		super();
-		this.left = left;
-		this.right = right;
-		this.top = top;
-		this.bottom = bottom;
-		this.near = near;
-		this.far = far;
-    this.zoom = 1;
+			super();
+			this.left = left;
+			this.right = right;
+			this.top = top;
+			this.bottom = bottom;
+			this.near = near;
+			this.far = far;
+			this.zoom = 1;
 
-		this.computeProjectionMatrix();
-	}
+			this.computeProjectionMatrix();
+		}
 
-  computeProjectionMatrix() {
-    const dx = ( this.right - this.left ) / ( 2 * this.zoom );
-		const dy = ( this.top - this.bottom ) / ( 2 * this.zoom );
-		const cx = ( this.right + this.left ) / 2;
-		const cy = ( this.top + this.bottom ) / 2;
+	  computeProjectionMatrix() {
+			const dx = ( this.right - this.left ) / ( 2 * this.zoom );
+			const dy = ( this.top - this.bottom ) / ( 2 * this.zoom );
+			const cx = ( this.right + this.left ) / 2;
+			const cy = ( this.top + this.bottom ) / 2;
 
-		let left = cx - dx;
-		let right = cx + dx;
-		let top = cy + dy;
-		let bottom = cy - dy;
+			let left = cx - dx;
+			let right = cx + dx;
+			let top = cy + dy;
+			let bottom = cy - dy;
 
-		this.projectionMatrix.orthographic(left, right, top, bottom, this.near, this.far);
+			this.projectionMatrix.orthographic(left, right, top, bottom, this.near, this.far);
   }
 
   override getCameraType() {
