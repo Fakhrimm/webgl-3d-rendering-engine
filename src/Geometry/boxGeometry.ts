ca790a9c2aa5e2ce9b315f3614ef56300869d5e6
Fakhrimm <13521045@mahasiswa.itb.ac.id>
2024-05-18 15:44:58 +0700

    feat: add smooth shading for bonus

--- src/Geometry/boxGeometry.ts
+++ src/Geometry/boxGeometry.ts
@@ -1,13 +1,12 @@
-import { BufferAttribute } from "./bufferAttribute";
-import { BufferGeometry } from "./bufferGeometry";
+import {BufferGeometry} from "./bufferGeometry";
 
 export class BoxGeometry extends BufferGeometry {
     width: number;
     height: number;
     depth: number;
 
-    constructor(width=1, height=1, depth=1) {
-        super();
+    constructor(width=1, height=1, depth=1, isSmoothShading = false) {
+        super(isSmoothShading);
         this.width = width;
         this.height = height;
         this.depth = depth;
@@ -15,75 +14,36 @@ export class BoxGeometry extends BufferGeometry {
         const hw = width / 2, hh = height / 2, hd = depth / 2;
 
         const vertices = new Float32Array([
-            // Front face
-            -hw, -hh,  hd, 1.0,  hw, -hh,  hd, 1.0,  hw,  hh,  hd, 1.0,
-            -hw, -hh,  hd, 1.0,  hw,  hh,  hd, 1.0, -hw,  hh,  hd, 1.0,
-            // Back face
-            hw, -hh, -hd, 1.0, -hw, -hh, -hd, 1.0, -hw,  hh, -hd, 1.0,
-            hw, -hh, -hd, 1.0, -hw,  hh, -hd, 1.0,  hw,  hh, -hd, 1.0,
-            // Top face
-            hw,  hh, hd, 1.0,  hw,  hh, -hd, 1.0,  -hw,  hh,  -hd, 1.0,
-            -hw, hh, -hd, 1.0,  -hw,  hh,  +hd, 1.0, +hw,  hh, +hd, 1.0,
-            // Bottom face
-            hw, -hh,  hd, 1.0,  -hw, -hh,  +hd, 1.0, -hw, -hh, -hd, 1.0,
-            -hw, -hh,  -hd, 1.0,  hw, -hh, -hd, 1.0, hw, -hh, hd, 1.0,
-            // Right face
-            hw, -hh,  hd, 1.0,  hw, -hh, -hd, 1.0,  hw,  hh, -hd, 1.0,
-            hw, -hh,  hd, 1.0,  hw,  hh, -hd, 1.0,  hw,  hh,  hd, 1.0,
-            // Left face
-            -hw, -hh, -hd, 1.0, -hw, -hh,  hd, 1.0, -hw,  hh,  hd, 1.0,
-            -hw, -hh, -hd, 1.0, -hw,  hh,  hd, 1.0, -hw,  hh, -hd, 1.0
+            +hw, +hh, +hd, 1,
+            -hw, +hh, +hd, 1,
+            -hw, -hh, +hd, 1,
+            +hw, -hh, +hd, 1,
+            +hw, +hh, -hd, 1,
+            -hw, +hh, -hd, 1,
+            -hw, -hh, -hd, 1,
+            +hw, -hh, -hd, 1,
         ]);
 
-        // Normals for each face
-        const normals = new Float32Array([
-            // Front
-            0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,
-            0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,
-            // Back
-            0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,
-            0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,
-            // Top
-            0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,
-            0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,
-            // Bottom
-            0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,
-            0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,
-            // Right
-            1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,
-            1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,
-            // Left
-            -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0,
-            -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0
+        const indices = new Uint16Array([
+            // front
+            0, 1, 2,
+            2, 3, 0,
+            // back
+            4, 7, 6,
+            6, 5, 4,
+            // top
+            0, 4, 5,
+            5, 1, 0,
+            // bottom
+            3, 2, 6,
+            6, 7, 3,
+            // right
+            0, 3, 7,
+            7, 4, 0,
+            // left
+            1, 5, 6,
+            6, 2, 1,
         ]);
-
-        // Texture coordinates for each vertex
-        const texCoords = new Float32Array([
-            0, 0,  1, 0,  1, 1,  0, 0,  1, 1,  0, 1,  // Front
-            1, 0,  0, 0,  0, 1,  1, 0,  0, 1,  1, 1,  // Back
-            0, 1,  1, 1,  1, 0,  0, 1,  1, 0,  0, 0,  // Top
-            0, 0,  1, 0,  1, 1,  0, 0,  1, 1,  0, 1,  // Bottom
-            1, 0,  0, 0,  0, 1,  1, 0,  0, 1,  1, 1,  // Right
-            0, 0,  1, 0,  1, 1,  0, 0,  1, 1,  0, 1   // Left
-        ]);
-
-        this.setAttribute('a_position', new BufferAttribute(vertices, 4));
-        this.setAttribute('a_texcoord', new BufferAttribute(texCoords, 2));
-        this.setAttribute('a_normal', new BufferAttribute(normals, 3));
-
-        this.calculateNormals();
-    }
-
-    static fromJSON(json: any): BoxGeometry {
-        return new BoxGeometry(json.width, json.height, json.depth);
-    }
-
-    public toJSON(): object {
-        return {
-            ...super.toJSON(),
-            a_width: this.width,
-            a_height: this.height,
-            a_depth: this.depth,
-        };
+        this.calculateAndSetAttributes(vertices, indices);
     }
 }
--- src/Geometry/bufferGeometry.ts
+++ src/Geometry/bufferGeometry.ts
@@ -1,11 +1,16 @@
 import { BufferAttribute } from './bufferAttribute'
+import {Vector3} from "../Math/vector-3.ts";
 
 export class BufferGeometry {
     private _attributes: {[name: string]: BufferAttribute};
+    private inputPosition?: Float32Array;
+    private inputIndices?: Uint16Array;
     private _indices?: BufferAttribute;
+    private isSmoothShading: boolean;
 
-    constructor() {
+    constructor(isSmoothShading = false) {
         this._attributes = {};
+        this.isSmoothShading = isSmoothShading;
     }
 
     get attributes() {
@@ -17,64 +22,142 @@ export class BufferGeometry {
         return this._indices;
     }
 
-
     setIndices(indices: BufferAttribute) {
         this._indices = indices;
         return this;
     }
 
-
-    removeIndices() {
-        this._indices = undefined;
-        return this;
-    }
-
-
     setAttribute(name: string, attribute: BufferAttribute) {
         this._attributes[name] = attribute;
         return this;
     }
-
-
-    getAttribute(name: string) {
-        return this._attributes[name];
+    setToSmoothShading() {
+        this.isSmoothShading = true;
+        this.calculateAndSetAttributes(this.inputPosition!, this.inputIndices!);
     }
 
-
-    deleteAttribute(name: string) {
-        delete this._attributes[name];
-        return this;
+    setToFlatShading() {
+        this.isSmoothShading = false;
+        this.calculateAndSetAttributes(this.inputPosition!, this.inputIndices!);
     }
 
-    calculateNormals(forceNewAttribute=false) {
-        const position = this.getAttribute('a_position');
-        if (!position) return;
-        let normal = this.getAttribute('a_normal');
-        if (forceNewAttribute || !normal)
-            normal = new BufferAttribute(new Float32Array(position.length), position.size);
-        // Lakukan kalkulasi normal disini.
-        this.setAttribute('a_normal', normal);
-    }
+    calculateAndSetAttributes(inputPosition: Float32Array, inputIndices: Uint16Array) {
+        this.inputPosition = inputPosition;
+        this.inputIndices = inputIndices;
 
-    static fromJSON(json: any): BufferGeometry {
-        const geometry = new BufferGeometry();
-        if (json.indices) {
-            geometry._indices = BufferAttribute.fromJSON(json.indices);
+        const position = new Float32Array(4 * inputIndices.length);
+        const normal = new Float32Array(3 * inputIndices.length);
+        const texcoord = new Float32Array(2 * inputIndices.length);
+
+        // Copy input to position
+        for (let i = 0; i < inputIndices.length; i++) {
+            const index = inputIndices[i];
+            position[i * 4] = inputPosition[index * 4];
+            position[i * 4 + 1] = inputPosition[index * 4 + 1];
+            position[i * 4 + 2] = inputPosition[index * 4 + 2];
+            position[i * 4 + 3] = inputPosition[index * 4 + 3];
         }
-        for (const name in json.attributes) {
-            geometry._attributes[name] = BufferAttribute.fromJSON(json.attributes[name]);
+
+        // Copy input to texcoord
+        for (let i = 0; i < inputIndices.length; i += 3) {
+            const i1 = inputIndices[i] * 2;
+            const i2 = inputIndices[i + 1] * 2;
+            const i3 = inputIndices[i + 2] * 2;
+
+            texcoord[i1] = 0;
+            texcoord[i1 + 1] = 0;
+            texcoord[i2] = 1;
+            texcoord[i2 + 1] = 0;
+            texcoord[i3] = 1;
+            texcoord[i3 + 1] = 1;
         }
-        return geometry;
+
+        if (!this.isSmoothShading) {
+            // Calculate face normals
+            for (let i = 0; i < inputIndices.length; i += 3) {
+                const i1 = inputIndices[i] * 4;
+                const i2 = inputIndices[i + 1] * 4;
+                const i3 = inputIndices[i + 2] * 4;
+
+                const v1 = new Vector3(inputPosition[i1], inputPosition[i1 + 1], inputPosition[i1 + 2]);
+                const v2 = new Vector3(inputPosition[i2], inputPosition[i2 + 1], inputPosition[i2 + 2]);
+                const v3 = new Vector3(inputPosition[i3], inputPosition[i3 + 1], inputPosition[i3 + 2]);
+
+                const n = v2.clone().sub(v1).cross(v3.clone().sub(v1));
+
+                n.normalize();
+
+                for (let j = 0; j < 3; j++) {
+                    normal[3 * i + 3 * j] = n.x;
+                    normal[3 * i + 3 * j + 1] = n.y;
+                    normal[3 * i + 3 * j + 2] = n.z;
+                }
+            }
+        } else {
+            // https://stackoverflow.com/questions/45477806/general-method-for-calculating-smooth-vertex-normals-with-100-smoothness
+            // Calculate vertex normals
+            const vertexNormals: Vector3[] = new Array(inputPosition.length / 4)
+            for (let i = 0; i < vertexNormals.length; i++) {
+                vertexNormals[i] = new Vector3(0, 0, 0);
+            }
+            const vertexFaces: number[] = new Array(inputPosition.length / 4).fill(0);
+
+            for (let i = 0; i < inputIndices.length; i += 3) {
+
+                const i1 = inputIndices[i];
+                const i2 = inputIndices[i + 1];
+                const i3 = inputIndices[i + 2];
+
+                const v1 = new Vector3(inputPosition[i1 * 4], inputPosition[i1 * 4 + 1], inputPosition[i1 * 4 + 2]);
+                const v2 = new Vector3(inputPosition[i2 * 4], inputPosition[i2 * 4 + 1], inputPosition[i2 * 4 + 2]);
+                const v3 = new Vector3(inputPosition[i3 * 4], inputPosition[i3 * 4 + 1], inputPosition[i3 * 4 + 2]);
+
+                const n = v2.clone().sub(v1).cross(v3.clone().sub(v1));
+                n.normalize();
+
+                const angle1 = v2.clone().sub(v1).angleTo(v3.clone().sub(v1));
+                const angle2 = v3.clone().sub(v2).angleTo(v1.clone().sub(v2));
+                const angle3 = v1.clone().sub(v3).angleTo(v2.clone().sub(v3));
+
+                const res1 = n.clone().multiplyScalar(angle1)
+                const res2 = n.clone().multiplyScalar(angle2)
+                const res3 = n.clone().multiplyScalar(angle3)
+
+                vertexNormals[i1].add(res1)
+                vertexNormals[i2].add(res2);
+                vertexNormals[i3].add(res3);
+
+                vertexFaces[i1]++;
+                vertexFaces[i2]++;
+                vertexFaces[i3]++;
+            }
+
+            for (let i = 0; i < vertexNormals.length; i++) {
+                if (vertexFaces[i] === 0) {
+                    continue;
+                }
+                vertexNormals[i].divideScalar(vertexFaces[i]);
+                vertexNormals[i].normalize();
+            }
+
+            for (let i = 0; i < inputIndices.length; i++) {
+                const index = inputIndices[i];
+                normal[i * 3] = vertexNormals[index].x;
+                normal[i * 3 + 1] = vertexNormals[index].y;
+                normal[i * 3 + 2] = vertexNormals[index].z;
+            }
+        }
+
+        this.setAttribute('a_position', new BufferAttribute(position, 4))
+        this.setAttribute('a_normal', new BufferAttribute(normal, 3));
+        this.setAttribute('a_texcoord', new BufferAttribute(texcoord, 2));
     }
 
-    public toJSON(): object {
-        const attributes: {[name: string]: object} = {};
-        for (const name in this._attributes) {
-            attributes[name] = BufferAttribute.toJSON(this._attributes[name]);
-        }
+    toJSON() {
         return {
-            attributes: attributes,
-            indices: this._indices ? BufferAttribute.toJSON(this._indices) : undefined,
-        };
+            isSmoothShading: this.isSmoothShading,
+            inputPosition: this.inputPosition!,
+            inputIndices: this.inputIndices!,
+        }
     }
 }
