6454226cbce0767c879eaaa926f2f3c5747d0476
louis <louiscaesapdf@gmail.com>
2024-05-25 20:46:25 +0700

    update easing integration

--- src/Animation/animationRunner.ts
+++ src/Animation/animationRunner.ts
@@ -1,35 +1,108 @@
 import { Variables } from "../UI/Variables";
 import { AnimationClip, AnimationPath } from "./animation";
 
+// EASING FUNCTIONS
 function normal(t: any) {
     return t;
 }
 
-function easeInSine(t: any) {
-    return 1 - Math.cos((t * Math.PI) / 2);
+function easeInOutSine(t: any) {
+    return -(Math.cos(Math.PI * t) - 1) / 2;
 }
 
-function easeInQuad(t: any) {
-    return t * t;
+function easeInOutQuad(t: any) {
+    return t < 0.5 ? 2 * t * t : 1 - Math.pow(-2 * t + 2, 2) / 2;
 }
 
-function easeInCubic(t: any) {
-    return t * t * t;
+function easeInOutCubic(t: any) {
+    return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
 }
 
-function easeInQuart(t: any) {
-    return t * t * t * t;
+function easeInOutQuart(t: any) {
+    return t < 0.5 ? 8 * t * t * t * t : 1 - Math.pow(-2 * t + 2, 4) / 2;
 }
 
-function easeInExpo(t: any) {
-    return t === 0 ? 0 : Math.pow(2, 10 * (t - 1));
+function easeInOutQuint(t: any) {
+    return t < 0.5 ? 16 * t * t * t * t * t : 1 - Math.pow(-2 * t + 2, 5) / 2;
 }
 
-function easeInCirc(t: any) {
-    return 1 - Math.sqrt(1 - t * t);
+function easeInOutExpo(t: any) {
+    return t === 0
+        ? 0
+        : t === 1
+        ? 1
+        : t < 0.5 ? Math.pow(2, 20 * t - 10) / 2
+        : (2 - Math.pow(2, -20 * t + 10)) / 2;
 }
 
-function tween(startValue: any, endValue: any, duration: any, easingFunc: any, currentTime: any) {
+function easeInOutCirc(t: any) {
+    return t < 0.5
+        ? (1 - Math.sqrt(1 - Math.pow(2 * t, 2))) / 2
+        : (Math.sqrt(1 - Math.pow(-2 * t + 2, 2)) + 1) / 2;
+}
+
+function easeInOutBack(t: any) {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+    
+    return t < 0.5
+      ? (Math.pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
+      : (Math.pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2;
+}
+
+function easeInOutElastic(t: any) {
+    const c5 = (2 * Math.PI) / 4.5;
+    
+    return t === 0
+      ? 0
+      : t === 1
+      ? 1
+      : t < 0.5
+      ? -(Math.pow(2, 20 * t - 10) * Math.sin((20 * t - 11.125) * c5)) / 2
+      : (Math.pow(2, -20 * t + 10) * Math.sin((20 * t - 11.125) * c5)) / 2 + 1;
+}
+
+function easeOutBounce(t: any) {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+    
+    if (t < 1 / d1) {
+        return n1 * t * t;
+    } else if (t < 2 / d1) {
+        return n1 * (t -= 1.5 / d1) * t + 0.75;
+    } else if (t < 2.5 / d1) {
+        return n1 * (t -= 2.25 / d1) * t + 0.9375;
+    } else {
+        return n1 * (t -= 2.625 / d1) * t + 0.984375;
+    }
+}
+
+function easeInOutBounce(t: any) {
+    return t < 0.5
+      ? (1 - easeOutBounce(1 - 2 * t)) / 2
+      : (1 + easeOutBounce(2 * t - 1)) / 2;
+}
+
+const easingFunctions = {
+    "easeInOutSine": easeInOutSine,
+    "easeInOutQuad": easeInOutQuad,
+    "easeInOutCubic": easeInOutCubic,
+    "easeInOutQuart": easeInOutQuart,
+    "easeInOutQuint": easeInOutQuint,
+    "easeInOutExpo": easeInOutExpo,
+    "easeInOutCirc": easeInOutCirc,
+    "easeInOutBack": easeInOutBack,
+    "easeInOutElastic": easeInOutElastic,
+    "easeInOutBounce": easeInOutBounce,
+    "normal": normal
+};
+
+function tween(startValue: any, endValue: any, duration: any, easingFuncName: string, currentTime: any) {
+    const easingFunc = easingFunctions[easingFuncName as keyof typeof easingFunctions];
+    if (!easingFunc) {
+        throw new Error(`Unknown easing function: ${easingFuncName}`);
+    }
+
     const t = Math.min(currentTime/duration, 1); // Normalized time
     const easedT = easingFunc(t);
     return startValue + (endValue - startValue) * easedT;
@@ -141,6 +214,10 @@ export class AnimationRunner {
                 
                 this.deltaFrame %= 1;
                 this.updateSceneGraph();
+            }
+
+            if (this.currentFrame == this.length-1 && !this.isAuto) {
+                this.isPlaying = false;
             }
         }
     }
